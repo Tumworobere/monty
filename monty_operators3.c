19b6d67d843396e0f072bd917955aa3b95eb4176
Tumworobere <annaht.annah@gmail.com>
2022-12-17 01:32:57 +0300

    Implement the sub opcode.

--- monty_operators3.c
+++ monty_operators3.c
@@ -56,4 +56,3 @@ void f_nop(stack_t **head, unsigned int counter)
 	(void) counter;
 	(void) head;
 }
-
--- monty_operators4.c
+++ monty_operators4.c
@@ -53,3 +53,4 @@ void f_swap(stack_t **head, unsigned int counter)
 	h->n = h->next->n;
 	h->next->n = temp;
 }
+
